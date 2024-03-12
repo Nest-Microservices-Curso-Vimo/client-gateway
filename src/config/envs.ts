9394fe4a5c0874642d8d12606a62bce28334836a
Mateo Morales <vimofama@outlook.com>
2024-03-12 11:46:09 -0500

    Añadir módulo de orders y controlador de orders

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -5,13 +5,17 @@ interface EnvVars {
   PORT: number;
   PRODUCTS_MICROSERVICE_HOST: string;
   PRODUCTS_MICROSERVICE_PORT: number;
+  ORDERS_MICROSERVICE_HOST: string;
+  ORDERS_MICROSERVICE_PORT: number;
 }
 
 const envSchema = joi
   .object({
     PORT: joi.number().required(),
     PRODUCTS_MICROSERVICE_HOST: joi.string().required(),
-    PRODUCTS_MICROSERVICE_PORT: joi.number().required()
+    PRODUCTS_MICROSERVICE_PORT: joi.number().required(),
+    ORDERS_MICROSERVICE_HOST: joi.string().required(),
+    ORDERS_MICROSERVICE_PORT: joi.number().required(),
   })
   .unknown(true);
 
@@ -26,5 +30,7 @@ const envVars: EnvVars = value;
 export const envs = {
   port: envVars.PORT,
   productsMicroServiceHost: envVars.PRODUCTS_MICROSERVICE_HOST,
-  productsMicroServicePort: envVars.PRODUCTS_MICROSERVICE_PORT
+  productsMicroServicePort: envVars.PRODUCTS_MICROSERVICE_PORT,
+  ordersMicroServiceHost: envVars.ORDERS_MICROSERVICE_HOST,
+  ordersMicroServicePort: envVars.ORDERS_MICROSERVICE_PORT,
 };
