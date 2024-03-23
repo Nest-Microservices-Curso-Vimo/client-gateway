c4075c84823c3ebc0b25dc6ae881e60ad4e1993c
Mateo Morales <vimofama@outlook.com>
2024-03-23 14:13:26 -0500

    Agregado módulo de autenticación y controlador de autenticación

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller';
+import { NATS_SERVICES } from 'src/config';
+import { NatsModule } from 'src/transports/nats.module';
+
+@Module({
+  controllers: [AuthController],
+  providers: [],
+  imports: [NatsModule],
+})
+export class AuthModule {}
